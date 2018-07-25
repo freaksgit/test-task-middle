75afd22dc97a69103054a1819cf12c2f88670eb5
Vasyl Stoliarchuk <stoliarchuk.v.vasyl@gmail.com>
2018-07-25 12:35:59 +0300

    android:configChanges="orientation|screenSize" is added to <activity> for avoiding of Activity recreations on orientation changes

--- app/src/main/java/vasyl/v/stoliarchuk/testtaskmiddle/features/reposearch/RepoSearchPresenter.kt
+++ app/src/main/java/vasyl/v/stoliarchuk/testtaskmiddle/features/reposearch/RepoSearchPresenter.kt
@@ -3,10 +3,8 @@ package vasyl.v.stoliarchuk.testtaskmiddle.features.reposearch
 class RepoSearchPresenter(private val mvpView: RepoSearchContract.View) : RepoSearchContract.Presenter {
 
     override fun subscribe() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun unsubscribe() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
